c3c2a13f33c57c4010eecdfe260cfa9417733903
eladcn <elad.metal@gmail.com>
2020-02-11 22:05:54 +0200

    - Initial commit.

---
+++ coronavirus_ccp_predictions.py
@@ -0,0 +1,79 @@
+import operator
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error, r2_score
+
+from data_grabber import DataGrabber
+
+CASES_POLYNOMIAL_DEGREE = 6
+DEATHS_POLYNOMIAL_DEGREE = 6
+GRAB_DATA_FILE_FROM_SERVER = True
+DEFAULT_DATA_FILE_NAME = "dataset_2020-02-11.csv" # This will be used as the data file if GRAB_DATA_FILE_FROM_SERVER is set to False.
+
+def get_training_set():
+    grabber = DataGrabber()
+    data_file_name = DEFAULT_DATA_FILE_NAME
+
+    if GRAB_DATA_FILE_FROM_SERVER:
+        grabber.grab_data()
+        data_file_name = grabber.get_default_file_name()
+    
+    return np.genfromtxt(data_file_name, delimiter=',').astype(np.int32)
+
+def train_model(x, y, polynomial_degree=6):
+    polynomial_features = PolynomialFeatures(degree=polynomial_degree)
+    x_poly = polynomial_features.fit_transform(x)
+
+    model = LinearRegression()
+    model.fit(x_poly, y)
+
+    return model
+
+def get_predictions(x, model, polynomial_degree=6):
+    polynomial_features = PolynomialFeatures(degree=polynomial_degree)
+    x_poly = polynomial_features.fit_transform(x)
+
+    return model.predict(x_poly)
+
+def print_stats(model_name, y, y_pred):
+    rmse = np.sqrt(mean_squared_error(y, y_pred))
+    r2 = r2_score(y, y_pred)
+
+    print(model_name + " RMSE: " + str(rmse))
+    print(model_name + " R2: " + str(r2))
+
+def plot_graph(model_name, x, y, y_pred):
+    plt.scatter(x, y, s=10)
+    sort_axis = operator.itemgetter(0)
+    sorted_zip = sorted(zip(x, y_pred), key=sort_axis)
+    x, y_pred = zip(*sorted_zip)
+    plt.plot(x, y_pred, color='m')
+    plt.show()
+
+if __name__ == "__main__":
+    training_set = get_training_set()
+    x = training_set[:, 0].reshape(-1, 1)
+    y_cases = training_set[:, 1]
+    y_deaths = training_set[:, 2]
+
+    cases_model = train_model(x, y_cases, CASES_POLYNOMIAL_DEGREE)
+    deaths_model = train_model(x, y_deaths, DEATHS_POLYNOMIAL_DEGREE)
+
+    y_cases_pred = get_predictions(x, cases_model, CASES_POLYNOMIAL_DEGREE)
+    y_deaths_pred = get_predictions(x, deaths_model, DEATHS_POLYNOMIAL_DEGREE)
+
+    next_day_x = np.array([len(training_set)]).reshape(-1, 1)
+    next_day_cases_pred = get_predictions(next_day_x, cases_model, CASES_POLYNOMIAL_DEGREE)
+    next_day_deaths_pred = get_predictions(next_day_x, deaths_model, DEATHS_POLYNOMIAL_DEGREE)
+
+    print("The amount of sick in the next day will be: " + str(next_day_cases_pred))
+    print("The amount of deaths in the next day will be: " + str(next_day_deaths_pred))
+    print("")
+    print_stats("Cases", y_cases, y_cases_pred)
+    print_stats("Deaths", y_deaths, y_deaths_pred)
+
+    plot_graph("Cases", x, y_cases, y_cases_pred)
+    plot_graph("Deaths", x, y_deaths, y_deaths_pred)
---
+++ data_grabber.py
@@ -0,0 +1,67 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import csv
+
+""" 
+This class handles grabbing the data regarding CCP's coronavirus confirmed cases and deaths from the website:
+https://www.worldometers.info
+"""
+class DataGrabber:
+    TARGET_DOMAIN = "https://www.worldometers.info"
+
+    def grab_data(self, filename=''):
+        cases = self.get_cases()
+        deaths = self.get_deaths()
+
+        if filename == '':
+            filename = self.get_default_file_name()
+
+        self.save_data_to_file(filename, cases, deaths)
+
+    def get_cases(self):
+        cases_url = DataGrabber.TARGET_DOMAIN + "/coronavirus/coronavirus-cases/"
+        cases_request = requests.get(cases_url)
+        cases_content = cases_request.content
+
+        cases_soup = BeautifulSoup(cases_content, "html.parser")
+
+        cases = []
+        cases_table = cases_soup.select(".table-responsive")[2].find("table")
+        cases_table_rows = cases_table.select("tr")
+
+        for i in range(1, len(cases_table_rows)):
+            currentDeaths = int(cases_table_rows[i].find_next("td").find_next("td").string.replace(",", ""))
+            cases.append(currentDeaths)
+
+        return list(reversed(cases))
+    
+    def get_deaths(self):
+        deaths_url = DataGrabber.TARGET_DOMAIN + "/coronavirus/coronavirus-death-toll/"
+        deaths_request = requests.get(deaths_url)
+        deaths_content = deaths_request.content
+
+        deaths_soup = BeautifulSoup(deaths_content, "html.parser")
+
+        deaths = []
+        deaths_table = deaths_soup.select(".table-responsive")[0].find("table")
+        deaths_table_rows = deaths_table.select("tr")
+
+        for i in range(1, len(deaths_table_rows)):
+            currentDeaths = int(deaths_table_rows[i].find_next("td").find_next("td").string.replace(",", ""))
+            deaths.append(currentDeaths)
+
+        return list(reversed(deaths))
+
+    def save_data_to_file(self, filename, cases, deaths):
+        data = []
+
+        for i in range(0, len(cases)):
+            data.append([i, cases[i], deaths[i]])
+        
+        with open("datasets/" + filename, 'w', newline='') as f:
+            csv_writer = csv.writer(f, delimiter=',')
+            csv_writer.writerows(data)
+
+    def get_default_file_name(self):
+        return "dataset_" + datetime.today().strftime('%Y-%m-%d') + ".csv"
