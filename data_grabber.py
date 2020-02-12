623d912dda8a43e4a4ba413b219effbf09a7f7ce
eladcn <elad.metal@gmail.com>
2020-02-12 18:25:53 +0200

    - Fixed some code duplication in the DataGrabber class. - Made a few functions in the DataGrabber class private. - Changed the polynomial degree of the cases model from 6 to 5. - Changed the polynomial degree of the deaths model from 6 to 4. - The function print_stats in the main.py file now prints all the stats regarding a model (and plots the model's graph) instead of just printing the RMSE and R2 scores. - The main.py now prints the functions that describe the trained models. - Small corrections to the plots' titles since they were a little bit misleading.

--- data_grabber.py
+++ data_grabber.py
@@ -8,7 +8,7 @@ This class handles grabbing the data regarding CCP's coronavirus confirmed cases
 https://www.worldometers.info
 """
 class DataGrabber:
-    TARGET_DOMAIN = "https://www.worldometers.info"
+    TARGET_DOMAIN = "https://www.worldometers.info/coronavirus"
 
     def grab_data(self, filename=''):
         cases = self.get_cases()
@@ -17,43 +17,37 @@ class DataGrabber:
         if filename == '':
             filename = self.get_default_file_name()
 
-        self.save_data_to_file(filename, cases, deaths)
+        self.__save_data_to_file(filename, cases, deaths)
+
+    def __get_table_content(self, path, table_selector, table_index):
+        url = DataGrabber.TARGET_DOMAIN + "/" + path
+        req = requests.get(url)
+        content = req.content
+
+        soup = BeautifulSoup(content, "html.parser")
+
+        data = []
+        table = soup.select(table_selector)[table_index].find("table")
+        table_rows = table.select("tr")
+
+        # The first row in the table is ignored (because it's the table's titles row), so we need to start from the second row.
+        for i in range(1, len(table_rows)):
+            current_value = int(table_rows[i].find_next("td").find_next("td").string.replace(",", ""))
+            data.append(current_value)
+
+        return data
 
     def get_cases(self):
-        cases_url = DataGrabber.TARGET_DOMAIN + "/coronavirus/coronavirus-cases/"
-        cases_request = requests.get(cases_url)
-        cases_content = cases_request.content
+        data = self.__get_table_content("coronavirus-cases", ".table-responsive", 2)
 
-        cases_soup = BeautifulSoup(cases_content, "html.parser")
-
-        cases = []
-        cases_table = cases_soup.select(".table-responsive")[2].find("table")
-        cases_table_rows = cases_table.select("tr")
-
-        for i in range(1, len(cases_table_rows)):
-            current_cases = int(cases_table_rows[i].find_next("td").find_next("td").string.replace(",", ""))
-            cases.append(current_cases)
-
-        return list(reversed(cases))
+        return list(reversed(data))
     
     def get_deaths(self):
-        deaths_url = DataGrabber.TARGET_DOMAIN + "/coronavirus/coronavirus-death-toll/"
-        deaths_request = requests.get(deaths_url)
-        deaths_content = deaths_request.content
+        data = self.__get_table_content("coronavirus-death-toll", ".table-responsive", 0)
 
-        deaths_soup = BeautifulSoup(deaths_content, "html.parser")
+        return list(reversed(data))
 
-        deaths = []
-        deaths_table = deaths_soup.select(".table-responsive")[0].find("table")
-        deaths_table_rows = deaths_table.select("tr")
-
-        for i in range(1, len(deaths_table_rows)):
-            current_deaths = int(deaths_table_rows[i].find_next("td").find_next("td").string.replace(",", ""))
-            deaths.append(current_deaths)
-
-        return list(reversed(deaths))
-
-    def save_data_to_file(self, filename, cases, deaths):
+    def __save_data_to_file(self, filename, cases, deaths):
         data = []
 
         for i in range(0, len(cases)):
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 from data_grabber import DataGrabber
 
-CASES_POLYNOMIAL_DEGREE = 6
-DEATHS_POLYNOMIAL_DEGREE = 6
+CASES_POLYNOMIAL_DEGREE = 5
+DEATHS_POLYNOMIAL_DEGREE = 4
 GRAB_DATA_FILE_FROM_SERVER = True
 DEFAULT_DATA_FILE_NAME = "dataset_2020-02-11.csv" # This will be used as the data file if GRAB_DATA_FILE_FROM_SERVER is set to False.
 
@@ -23,7 +23,7 @@ def get_training_set():
     
     return np.genfromtxt("datasets/" + data_file_name, delimiter=',').astype(np.int32)
 
-def train_model(x, y, polynomial_degree=6):
+def train_model(x, y, polynomial_degree):
     polynomial_features = PolynomialFeatures(degree=polynomial_degree)
     x_poly = polynomial_features.fit_transform(x)
 
@@ -32,18 +32,43 @@ def train_model(x, y, polynomial_degree=6):
 
     return model
 
-def get_predictions(x, model, polynomial_degree=6):
+def get_predictions(x, model, polynomial_degree):
     polynomial_features = PolynomialFeatures(degree=polynomial_degree)
     x_poly = polynomial_features.fit_transform(x)
 
     return model.predict(x_poly)
 
-def print_stats(model_name, y, y_pred):
+def print_stats(model_name, model, x, y, polynomial_degree):
+    y_pred = np.round(get_predictions(x, model, polynomial_degree), 0).astype(np.int32)
+    next_day_x = np.array([len(x)]).reshape(-1, 1)
+    next_day_pred = np.round(get_predictions(next_day_x, model, polynomial_degree), 0)
+
+    print_model_polynomial(model_name, model)
+    print("The predicted amount of " + model_name + " in the next day is: " + str(int(next_day_pred)))
+
     rmse = np.sqrt(mean_squared_error(y, y_pred))
     r2 = r2_score(y, y_pred)
 
     print(model_name + " RMSE: " + str(rmse))
     print(model_name + " R2: " + str(r2))
+    print("")
+
+    plot_graph(model_name, x, y, y_pred)
+
+def print_model_polynomial(model_name, model):
+    coef = model.coef_
+    intercept = model.intercept_
+    poly = "{0:.3f}".format(intercept)
+
+    for i in range(1, len(coef)):
+        if coef[i] >= 0:
+            poly += " + "
+        else:
+            poly += " - "
+        
+        poly += "{0:.3f}".format(coef[i]).replace("-", "") + "X^" + str(i)
+
+    print("The " + model_name + " function is: f(X) = " + poly)
 
 def plot_graph(model_name, x, y, y_pred):
     plt.scatter(x, y, s=10)
@@ -51,7 +76,7 @@ def plot_graph(model_name, x, y, y_pred):
     sorted_zip = sorted(zip(x, y_pred), key=sort_axis)
     x, y_pred = zip(*sorted_zip)
     plt.plot(x, y_pred, color='m')
-    plt.title(model_name + " per day")
+    plt.title("Amount of " + model_name + " in each day")
     plt.xlabel("Day")
     plt.ylabel(model_name)
     plt.show()
@@ -65,18 +90,5 @@ if __name__ == "__main__":
     cases_model = train_model(x, y_cases, CASES_POLYNOMIAL_DEGREE)
     deaths_model = train_model(x, y_deaths, DEATHS_POLYNOMIAL_DEGREE)
 
-    y_cases_pred = get_predictions(x, cases_model, CASES_POLYNOMIAL_DEGREE)
-    y_deaths_pred = get_predictions(x, deaths_model, DEATHS_POLYNOMIAL_DEGREE)
-
-    next_day_x = np.array([len(training_set)]).reshape(-1, 1)
-    next_day_cases_pred = get_predictions(next_day_x, cases_model, CASES_POLYNOMIAL_DEGREE)
-    next_day_deaths_pred = get_predictions(next_day_x, deaths_model, DEATHS_POLYNOMIAL_DEGREE)
-
-    print("The amount of sick in the next day will be: " + str(next_day_cases_pred))
-    print("The amount of deaths in the next day will be: " + str(next_day_deaths_pred))
-    print("")
-
-    print_stats("Cases", y_cases, y_cases_pred)
-    print_stats("Deaths", y_deaths, y_deaths_pred)
-    plot_graph("Cases", x, y_cases, y_cases_pred)
-    plot_graph("Deaths", x, y_deaths, y_deaths_pred)
+    print_stats("Cases", cases_model, x, y_cases, CASES_POLYNOMIAL_DEGREE)
+    print_stats("Deaths", deaths_model, x, y_deaths, DEATHS_POLYNOMIAL_DEGREE)
