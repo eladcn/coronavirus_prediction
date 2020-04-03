4954bb9fd1c5b6a69f94f5de77eaea4b5709d86c
eladcn <elad.metal@gmail.com>
2020-04-03 12:18:03 +0300

    - Changed the deaths model's polynomial degree back to 5, it seems to be more accurate. - The DeathsDataGrabber class now also uses the GraphsDataGrabber class. - Updated the datasets. - Updated the predictions table.

--- data_grabbers/deaths_data_grabber.py
+++ data_grabbers/deaths_data_grabber.py
@@ -1,4 +1,5 @@
 from data_grabbers.data_grabber import DataGrabber
+from data_grabbers.graphs_data_grabber import GraphsDataGrabber
 
 class DeathsDataGrabber(DataGrabber):
     DATASET_PREFIX = "deaths"
@@ -7,15 +8,12 @@ class DeathsDataGrabber(DataGrabber):
         super()
 
     def grab_data(self):
-        data = self.__get_deaths()
+        graphs_grabber = GraphsDataGrabber()
+
+        data = graphs_grabber.get_data("coronavirus-death-toll")
         filename = self.get_dataset_file_name()
 
         self.save_data_to_file(filename, data)
-
-    def __get_deaths(self):
-        data = self.get_table_content("coronavirus-death-toll", ".table-responsive", 0)
-
-        return list(reversed(data))
 
     def get_dataset_file_name(self, dataset_date=""):
         return super().get_dataset_file_name(DeathsDataGrabber.DATASET_PREFIX, dataset_date=dataset_date)
